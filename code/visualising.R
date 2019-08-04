3dce0cceaa927a6bc2f8861f8fa1874c3ff7b8c7
Peter McNamara <88451999@student.uts.edu.au>
2019-08-04 13:54:20 +1000

    More changes

--- code/visualising.R
+++ code/visualising.R
@@ -165,3 +165,24 @@ pitch_custom <- list(
 ggplot() +
   annotate_pitch(dimensions = pitch_custom) +
   theme_pitch()
+
+
+
+coords = matrix(c(78.46801, 19.53407,
+               78.46801, 19.74557,
+               78.83157, 19.74557,
+               78.83157, 19.53407,
+               78.46801, 19.53407), 
+             ncol = 2, byrow = TRUE)
+
+coords = matrix(c(0, 0,
+                  0, 75,
+                  110, 75,
+                  110, 0,
+                  0, 0), 
+                ncol = 2, byrow = TRUE)
+
+
+P1 = Polygon(coords)
+Ps1 = SpatialPolygons(list(Polygons(list(P1), ID = "a")) )#, proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+plot(Ps1, axes = TRUE)
